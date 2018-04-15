d519821abf6dc88a8d49c89cf7036b3f3d89dfd3
Karpovusa <karpovusa@gmail.com>
2018-04-15 22:57:10 +0300

    доработки архитектуры

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase{
+
+    public GroupHelper(FirefoxDriver wd) {
+        super(wd);
+    }
+
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
+    }
+
+    public void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        click(By.xpath("//div[@id='content']/form"));
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+        click(By.name("group_header"));
+        click(By.name("group_name"));
+        click(By.name("group_header"));
+    }
+
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
+
+    public void selectGroup() {
+        click(By.name("selected[]"));
+    }
+}
